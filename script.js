cf70101a9d37fa0473b1e7d198834cbe0517660a
Sulleh1992 <ben.sullivan1992@gmail.com>
2024-02-19 18:21:48 +0000

    Code Clean-Up + Capture Function
    Code tidied up and new function to capture input created to ensure up-to-date values are returned.

--- script.js
+++ script.js
@@ -1,88 +1,112 @@
-//Listen for changes to units value to enable their fields and change their respective placeholders, step and min/max values according to the unit selected.
+//Define function to listen for changes to height units to enable their fields and change their respective placeholders, step and min/max values according to the unit selected.
+const 
+    heightInput = document.getElementById("height"),
+    selectHeightUnit = document.getElementById("heightUnit"),
+    checkHeightUnit = () => {
+        selectHeightUnit.addEventListener("change", function() {
+            heightInput.value = "";
+            switch(selectHeightUnit.value) {
+                case "metres":
+                    heightInput.removeAttribute("disabled");
+                    heightInput.placeholder = "Height (m)";
+                    break;
+                case "centimetres":
+                    heightInput.removeAttribute("disabled");
+                    heightInput.placeholder = "Height (cm)";
+                    break;
+                case "inches":
+                    heightInput.removeAttribute("disabled");
+                    heightInput.placeholder = "Height (in)";
+                    break;
+            }
+        })
+    }
+;
+
+//Define function to listen for changes to weight units to enable their fields and change their respective placeholders, step and min/max values according to the unit selected.
+const
+    weightInput = document.getElementById("weight"),
+    selectWeightUnit = document.getElementById("weightUnit"),
+    checkWeightUnit = () => {
+        selectWeightUnit.addEventListener("change", function() {
+            weightInput.value = "";
+            switch(selectWeightUnit.value) {
+                case "kilograms":
+                    weightInput.removeAttribute("disabled");    
+                    weightInput.placeholder = "Weight (kg)";
+                    break;
+                case "pounds":
+                    weightInput.removeAttribute("disabled");
+                    weightInput.placeholder = "Weight (lbs)";
+                    break;
+            };
+        });
+    }
+;
+
+//Start listening for Unit changes on DOMContentLoad 
 document.addEventListener("DOMContentLoaded", function() {
-    let heightInput = document.getElementById("height");
-    let heightUnit = document.getElementById("heightUnit");
-    heightUnit.addEventListener("change", function() {
-        heightInput.value = "";
-        switch(heightUnit.value) {
-            case "metres":
-                heightInput.removeAttribute("disabled");
-                heightInput.placeholder = "Height (m)";
-                heightInput.min = "1";
-                heightInput.max = "2.5";
-                heightInput.step = "0.01";
-                break;
-            case "centimetres":
-                heightInput.removeAttribute("disabled");
-                heightInput.placeholder = "Height (cm)";
-                heightInput.min = "100";
-                heightInput.max = "250";
-                heightInput.step = "1";
-                break;
-            case "inches":
-                heightInput.removeAttribute("disabled");
-                heightInput.placeholder = "Height (in)";
-                heightInput.min = "40";
-                heightInput.max = "100";
-                heightInput.step = "0.25";
-                break;
-        }
-    });
-    let weightInput = document.getElementById("weight");
-    let weightUnit = document.getElementById("weightUnit");
-    weightUnit.addEventListener("change", function() {
-        switch(weightUnit.value) {
-            case "kilograms":
-                weightInput.placeholder = "Weight (kg)";
-                weightInput.removeAttribute("disabled");
-                break;
-            case "pounds":
-                weightInput.placeholder = "Weight (lbs)";
-                weightInput.removeAttribute("disabled");
-                break;
-        }
-    });
+    checkHeightUnit();
+    checkWeightUnit();
 });
 
-//On clicking "Submit", use values provided to calculate BMI.
-function calculateBmi() {
-    let result, height, weight;
-    let output = document.getElementById("output");
-    const heightUnit = document.getElementById("heightUnit").value;
-    const heightInput = parseFloat(document.getElementById("height").value);
-    const weightUnit = document.getElementById("weightUnit").value;
-    const weightInput = parseFloat(document.getElementById("weight").value);
+//Define function to capture inputted values.
+const getValues = () => {
+    return{
+        age: parseInt(document.getElementById("age").value),
+        heightValue: heightInput.value,
+        heightUnit: selectHeightUnit.value,
+        weightValue: weightInput.value,
+        weightUnit: selectWeightUnit.value
+    };
+};
 
-    //Convert height to metres for calculation.
-    if (heightUnit === "metres"){
-        height = heightInput;
-    } else if (heightUnit === "centimetres"){
-        height = heightInput/100;
-    } else {    //i.e. unit is inches.
-        height = heightInput*2.54;
-    }
+//Define function to use values provided to calculate BMI.
+const calculateBmi = () => {
+    let heightInMetres, weightInKilograms;
+    const output = document.getElementById("output");
+    const { age, heightValue, heightUnit, weightValue, weightUnit } = getValues();
 
-    //Convert weight to kilograms for calculation.
-    if (weightUnit === "kilograms"){
-        weight = weightInput;
-    } else {    //i.e. unit is pounds.
-        weight = weightInput/2.205;
-    }
-
-    //BMI only validated in adults, if age<18, do not return BMI.
-    if (parseInt(document.getElementById("age").value)<18){
-        output.innerHTML = "BMI should not be used in under 18-year-olds."
-        return;
+    //BMI only validated in adults so, if age<18, do not return BMI.
+    if (age<18){
+        output.innerHTML = "BMI should not be used in under 18-year-olds.";
     } else {
-        result = weight/height**2;
-    }
 
-    //At extremes of BMI, suggest to check input values.
-    if (result<10) {
-        output.innerHTML = "BMI is less than 10kg/m&sup2, please check input values.";
-    } else if (result>60) {
-        output.innerHTML = "BMI is over 60kg/m&sup2, please check input values.";
-    } else {
-    output.innerHTML = "BMI is " + result.toFixed(1) + "kg/m&sup2";
-    }
-}
+        //Convert height to metres for calculation.
+        const 
+            centimetresToMetresDivisor = 100,
+            inchesToMetresFactor = 2.54;
+
+        if (heightUnit === "centimetres"){
+            heightInMetres = heightValue/centimetresToMetresDivisor;
+        } else if (heightUnit === "inches"){
+            heightInMetres = heightValue*inchesToMetresFactor;
+        } else {    //i.e. height is already in metres.
+            heightInMetres = heightValue;
+        }
+
+        //Convert weight to kilograms for calculation.
+        const poundsToKilogramsDivisor = 2.205;
+        if (weightUnit === "pounds"){
+            weightInKilograms = weightValue/poundsToKilogramsDivisor;
+        } else {    //i.e. weight is already in kilograms.
+            weightInKilograms = weightValue;
+        }
+
+        //Calculate BMI.
+        const result = weightInKilograms/heightInMetres**2;
+
+        //At extremes of BMI, suggest to check input values.
+        if (result<10) {
+            output.innerHTML = "BMI is less than 10kg/m&sup2, please check input values.";
+        } else if (result>60) {
+            output.innerHTML = "BMI is over 60kg/m&sup2, please check input values.";
+        } else {
+            output.innerHTML = "BMI is " + result.toFixed(1) + "kg/m&sup2";
+        }
+    }   
+};
+
+//Define button variable and run calculator on click.
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", calculateBmi);
